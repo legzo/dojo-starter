feca8df295b50a979359aa0fa79e6a86b97247e6
Julien Terrier <julien.terrier@orange.com>
2017-11-24 17:48:12 +0100

    🤸 Init

---
+++ src/main/java/com/orange/coderoom/Dummy.java
@@ -0,0 +1,9 @@
+package com.orange.coderoom;
+
+public class Dummy {
+
+    public String getGreeting() {
+        return "Hello CodeRoom!";
+    }
+
+}
---
+++ src/test/java/com/orange/coderoom/DummyTest.java
@@ -0,0 +1,14 @@
+package com.orange.coderoom;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+    @Test
+    public void shouldFail() {
+        assertThat(false).isTrue();
+    }
+
+}
